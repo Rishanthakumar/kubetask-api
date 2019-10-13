51b63e8219edfbcea0010f15ff01a3851569497d
rishanthakumar <rishanthakumar@gmail.com>
2019-10-13 19:40:33 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kubetask-api
---
+++ kubetask-api/Data/KubeTaskContext.cs
@@ -0,0 +1,23 @@
+﻿using kubetask_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kubetask_api.Data
+{
+    public class KubeTaskContext: DbContext
+    {
+        public KubeTaskContext(DbContextOptions<KubeTaskContext> options) : base(options) { }
+
+        public DbSet<KTask> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToContainer("Users");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
